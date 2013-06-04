f1d6bfef6ba3b3aa05c9863d52670ef2643fb6db
alexssb <geniofuturo@gmail.com>
2013-06-03 23:43:14 -0300

    Defining class engine, which will make the translation process.

--- trans.py
+++ trans.py
@@ -1,2 +1,10 @@
+# coding: utf-8
+
 import urllib2, urllib
 
+## https://translate.google.com.br/translate_a/t
+## ?
+## client=t&hl=pt-BR&sl=en&tl=pt&ie=UTF-8&oe=UTF-8&multires=1&prev=btn&ssel=0&tsel=0&sc=1&q=home
+
+class Engine(object):
+    pass

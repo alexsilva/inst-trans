1c88db4485144fcd0710ccffa26a91d959846c91
alexssb <geniofuturo@gmail.com>
2013-06-04 03:19:26 -0300

    Implementing test routines.

--- tests.py
+++ tests.py
@@ -1,13 +1,23 @@
 # coding: utf-8
 
-from trans import Engine
+from trans import Engine, Groups
 from config import Config, Translation
 
 # ---------------------------------------------------------------------------------------------------------------------
-translation = Translation("book", "en", "pt")
+translation = Translation("look", "en", "pt")
 config = Config( translation )
 
 engine = Engine( config )
 # ---------------------------------------------------------------------------------------------------------------------
 
-print engine.trans()
+r =  engine.trans()
+print r
+print
+print r[0]
+print
+print r[1]
+print
+
+groups = Groups(r)
+
+print groups.related

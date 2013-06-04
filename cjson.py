dcb13fc4f19cf63090a265e475e230b56035a040
alexssb <geniofuturo@gmail.com>
2013-06-04 01:38:26 -0300

    Creating class Json for correction and decoding data in json.

---
+++ cjson.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+import re
+import json
+
+# ---------------------------------------------------------------------------------------------------------------------
+class Json(object):
+    def __init__(self, data):
+        self.data = data
+
+    def _clean(self):
+        ## re.search(",/s+,", self.data, re.DOTALL)
+
+        while (self.data.find(",,") > 0):
+            self.data = self.data.replace(",,")
+
+    def decode(self):
+        self._clean()
+
+        return json.loads( self.data )

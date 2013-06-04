1881a10105484fb0b73e54745af172ae37df7ef5
alexssb <geniofuturo@gmail.com>
2013-06-04 03:18:56 -0300

    Adding class to group the data.

--- trans.py
+++ trans.py
@@ -4,6 +4,51 @@ import urllib2, urllib
 from config import Config
 from cjson import Json
 
+# ---------------------------------------------------------------------------------------------------------------------
+class Groups(object):
+    def __init__(self, data):
+        self.data = data
+
+    def simple(self):
+        try:
+            d = self.data[0][0]
+            d = [i for i in d if i]
+        except IndexError:
+            d = []
+        d.reverse()
+        return d
+
+    def complex(self):
+        rel = {}
+        try: d = self.data[1]
+        except IndexError:
+            d = []
+        for i in d:
+            if type(i) is list:
+                try:
+                    if isinstance(i[0], (str, unicode)):
+                        rel[i[0]] = i[1]
+                except IndexError:
+                    continue
+                try:
+                    details = {}
+                    for defs in i[-1]:
+                        try:
+                            word, _deflist = defs[0], defs[1]
+                            details[word] = _deflist
+                        except IndexError:
+                            continue
+                except Exception:
+                    continue
+                rel[i[0]].append({"details": details})
+        return rel
+
+    @property
+    def related(self):
+        rel = {}
+        rel["simple"] = self.simple()
+        rel["class"] = self.complex()
+        return rel
 
 # ---------------------------------------------------------------------------------------------------------------------
 class Engine(object):

5394b757e285e7aab8f3fad5a1f9649355a222bb
alexssb <geniofuturo@gmail.com>
2013-06-06 02:40:45 -0300

    Adding supports at attributes of html.

--- html/tag.py
+++ html/tag.py
@@ -8,14 +8,13 @@ class Tag(object):
     content:
     """
 
-    _str_repr = '<{type}{space}{class}{space}{id}{style}>{eol}{content}{eol}</{type}>'
-    _unicode_repr = u'<{type}{space}{class}{space}{id}{space}{style}>{eol}{content}{eol}</{type}>'
+    _str_repr = '<{type}{space}{attr}{space}{style}>{eol}{content}{eol}</{type}>'
+    _unicode_repr = u'<{type}{space}{attr}{space}{space}{style}>{eol}{content}{eol}</{type}>'
 
     def __init__(self, **params):
         self.params = params
         self.params['content'] = params.get('content', [])
-        self.params['class'] = params.get('class', '')
-        self.params['id'] = params.get('id', '')
+        self.params['attr'] = params.get('attr', {})
         self.params['style'] = params.get('style', "")
         self.params['space'] = params.get('space', " ")
         self.params['eol'] = params.get('eol', "\n")
@@ -28,15 +27,20 @@ class Tag(object):
 
     def __convert(self, _u_str, method):
         if type(self.params['content']) is list:
-            self.params['content'] = [method(item) for item in self.params['content']]
+            self.params['content'] = [method(i) for i in self.params['content']]
             self.params['content'] = _u_str.join(self.params['content'])
+
+    def __group(self, _u_str, method):
+        self.params['attr'] = _u_str.join([method(i) for i in self.params['attr'].items()])
 
     def __str__(self):
         self.__convert('', str)
+        self.__group(' ', lambda args: '{0}="{1}"'.format(*args))
         return self._str_repr.format(**self.params)
 
     def __unicode__(self):
-        self.__convert(u'', lambda v: u'%s' % v)
+        self.__convert(u'', lambda v: u'{0}'.format(v))
+        self.__group(u' ', lambda args: u'{0}="{1}"'.format(*args))
         return self._unicode_repr.format(**self.params)
 
 # ---------------------------------------------------------------------------------------------------------------------

3a3e8c4c3413dff525537fc948609af1612a629c
alexssb <geniofuturo@gmail.com>
2013-06-06 02:41:56 -0300

    Redefining tests for.

--- html/tests.py
+++ html/tests.py
@@ -14,10 +14,25 @@ tag = Tag(type='p', content='python is the best', style=style)
 
 print style
 print tag
+print
 
 # tag inside tag
-li = [Tag(type='li', content='python is the best - %s'%i, style=style) for i in range(10)]
-
+li = [Tag(type='li', content='python is the best - %s'%i, style=style) for i in range(3)]
 tag = Tag(type='ul', content=li, style=style)
 
+print tag
+print
+print
+
+# tag groups chain
+tag = Tag(type='div', content=[], style=style)
+tag['content'].append(Tag(type='p', content='hello!'))
+
+print tag
+print
+
+# Adding Attribute
+tag = Tag(type='div', content=Tag(type='a', attr={'href': 'goto1'}, content='i am p tag!'), style=style)
+tag['attr']['class'] = 'main-div'
+tag['attr']['id'] = '#main-div'
 print tag

e33b777835eaaa8c14c0e5a3eb0e4302456c00b2
alexssb <geniofuturo@gmail.com>
2013-06-06 23:49:48 -0300

    Creating cache system using Django framework resource.

---
+++ db/__init__.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+__author__ = 'alex'
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
---
+++ db/models.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+from django.db import models
+
+class Translation(models.Model):
+    """
+    source:
+    target:
+    sourceLocale:
+    targetLocale:
+    """
+    source = models.CharField(max_length=255, unique=True)
+    target = models.CharField(max_length=255)
+
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    sourceLocale = models.CharField(max_length=25)
+    targetLocale = models.CharField(max_length=25)
+
+    class Meta(object):
+        app_label = "db"
+
+    @property
+    def simple(self):
+        return [self.source, self.target]
+
+    def __unicode__(self):
+        return u"{0}: {1}".format(self.source, self.target)
+
+class GrammaticalClass(models.Model):
+    """
+    name:
+    words:
+    """
+    name = models.CharField(max_length=255)
+    translation = models.ForeignKey("Translation")
+
+    def __unicode__(self):
+        return self.name
+
+class Word(models.Model):
+    """
+    word:
+    """
+    word = models.CharField(max_length=255)
+    grammaticalClass = models.ForeignKey("GrammaticalClass")
+
+    def __unicode__(self):
+        return self.word
